161750c8cbd0c8cb898973a652e73adff39b2ea5
lakshya-aga <lakshya001@ntu.edu.sg>
2026-05-31 08:06:26 +0800

    smilo and monsters

--- C_Smilo_and_Monsters.cpp
+++ C_Smilo_and_Monsters.cpp
@@ -1,18 +1,51 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <string>
 #include <algorithm>
-#include <numeric>
-#include <iomanip>
 using namespace std;
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout<<1<<endl;
+int main()
+{
+    // std::cout<<"Hello World";
+    long long t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(long long i=0; i<n; i++){
+            cin>>arr[i];
+        }
+        sort(arr.begin(), arr.end());
+        long long left_idx = 0; 
+        long long right_idx = n-1;
+        long long combo=0;
+        long long ans=0;
+        while(left_idx<right_idx){
+            long left = arr[left_idx];
+            long right = arr[right_idx];
+            if(left+combo<=right){
+                combo+=left;
+                ans+=left;
+                arr[left_idx++] = 0;
+            }
+            else{
+                arr[left_idx] = left - (right-combo);
+                ans+=(right-combo);
+                combo=right;
+                arr[right_idx--]=0;
+                ans++;
+                combo=0;
+            }
+            // cout<<ans<<" "<<left_idx<<" "<<right_idx<<endl;
+        }
+        if(left_idx == right_idx){
+            
+            ans+=(arr[left_idx]+1-combo)/2;
+            arr[left_idx] = arr[left_idx] - (arr[left_idx]+1-combo)/2;
+            if(arr[left_idx]!=0)
+            ans++;
+        }
+        cout<<ans<<endl;
+        }
     
 
     return 0;
